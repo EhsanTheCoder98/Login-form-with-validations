afad3505778439cf1d3a3bb3be2501f44f6262d8
Ethan Fathalipour <www.ehsanfathalipour110@gmail.com>
2023-09-05 02:06:12 +0330

    UI completed

--- src/Signup.js
+++ src/Signup.js
@@ -7,28 +7,28 @@ const Signup = () => {
       <form>
         <h2>Sign Up</h2>
         <div>
-          <label>Name </label>
+          <label    for="name">Name </label>
           <br/>
           <input type="text" id="name" name="name" />
         </div>
         <div>
-          <label>Email </label>
+          <label    for="email">Email </label>
           <br/>
           <input type="text" id="email" name="email" />
         </div>
         <div>
-          <label>Password </label>
+          <label    for="password">Password </label>
           <br/>
           <input type="password" id="password" name="password" />
         </div>
         <div>
-          <label>ConfirmPassword </label>
+          <label    for="ConfirmPassword">ConfirmPassword </label>
           <br/>
           <input type="password" id="ConfirmPassword" name="ConfirmPassword" />
         </div>
         <div className={styles.last}>
-          <label>Accept our terms </label>
-          <input type="checkbox" id="ConfirmPassword" name="ConfirmPassword" />
+          <label    for="Accept">Accept our terms </label>
+          <input type="checkbox" id="Accept" name="Accept" />
         </div>
         <button className={styles.login}>Login</button>
         <button type="submit" className={styles.submit}>Submit</button>
